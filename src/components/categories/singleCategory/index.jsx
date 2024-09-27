665be78b3131a1ea38a4d01e6562a503b35b0da5
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-09-27 14:35:42 +0400

    save changes

--- src/components/categories/singleCategory/index.jsx
+++ src/components/categories/singleCategory/index.jsx
@@ -3,15 +3,16 @@ import shoulderBag from "../../../assets/home/images/shoulderBag.png"
 
 const SingleCategory = (props) => {
   return (
-    <div className=' bg-[#F5F5F5] rounded-[8px] w-[264px] h-[162px] relative'>
-        <h1 className='ml-6 mt-6 text-[18px] font-medium'>{props.category.title}</h1>
+    <div className=" bg-[#F5F5F5] rounded-[8px] w-[264px] h-[162px] relative overflow-hidden">
+      <h1 className="ml-6 mt-6 text-[18px] font-medium">
+        {props.category.title}
+      </h1>
 
-        <div className='w-[163px] h-[157px] overflow-hidden ml-auto absolute right-0 bottom-0'>
-            <img src={props.category?.coverImage} alt="shoulderBag" />
-        </div>
-
+      <div className="w-[70%]  absolute right-[-34px] bottom-[-80px]">
+        <img src={props.category?.coverImage} alt="shoulderBag" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default SingleCategory
