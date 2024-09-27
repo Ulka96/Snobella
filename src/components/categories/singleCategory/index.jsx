847a15b6c0790d38066a5b0d718edd794d485d42
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-09-27 14:38:31 +0400

    save changes

--- src/components/categories/singleCategory/index.jsx
+++ src/components/categories/singleCategory/index.jsx
@@ -3,13 +3,17 @@ import shoulderBag from "../../../assets/home/images/shoulderBag.png"
 
 const SingleCategory = (props) => {
   return (
-    <div className=" bg-[#F5F5F5] rounded-[8px] w-[264px] h-[162px] relative overflow-hidden">
+    <div className=" bg-[#F5F5F5] rounded-[8px] w-[264px] h-[162px] relative ">
       <h1 className="ml-6 mt-6 text-[18px] font-medium">
         {props.category.title}
       </h1>
 
       <div className="w-[70%]  absolute right-[-34px] bottom-[-80px]">
-        <img src={props.category?.coverImage} alt="shoulderBag" />
+        <img
+          src={props.category?.coverImage}
+          alt="shoulderBag"
+          className="overflow-hidden"
+        />
       </div>
     </div>
   );
